1c4ae18e0ec33fbe5d3aab030913679c6438ac51
Michael Bain <michaelbain@Michaels-MacBook-Pro.local>
2013-01-22 11:36:43 -0500

    Added first files to the repo.

---
+++ Bain_Michael_assignment3.js
@@ -0,0 +1,43 @@
+//alert("JavaScript works!");
+
+// Michael Bain
+// SDI 1301
+// Project 3
+// Javascript
+// 1/20/13
+
+
+// My Variable
+
+
+
+
+
+
+//Object Method
+// string return and accessor
+var meSkater = {
+    "name": "Michael",
+    "age": 28,
+    "specialty": "street",
+    "payMethod": "visa",
+    "getPayMethod": function (payMethod) { 
+    return payMethod;                      
+    }   
+};
+console.log("I thought my hotel reservations were confirmed but I guess there was a problem.")
+console.log("The hotel wants to know which credit card I will pay with.")
+console.log("I will be using a " + meSkater.payMethod + ".")
+
+
+
+
+
+
+
+
+
+
+
+
+
