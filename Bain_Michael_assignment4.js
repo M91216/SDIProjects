edad94aee4a53adf10967fd39c441b9141af99ef
Michael Bain <michaelbain@Michaels-MacBook-Pro.local>
2013-01-30 16:05:54 -0500

    email validate add

--- Bain_Michael_assignment4.js
+++ Bain_Michael_assignment4.js
@@ -1,1 +1,69 @@
-alert("JavaScript works!");
+// Michael Bain
+// SDI project 4
+// 1/30/13
+
+var myLibrary = function() {
+
+
+//EMAIL VALIDATION
+function validEmail(email) {
+    
+var invalidChars = " /:,;";
+    
+    if (email ==""){       
+      return false;
+    }
+    for (var k=0; k<invalidChars.length; k++) {
+      var badChar = invalidChars.charAt(k);
+      if (email.indexOf(badChar) > -1){
+      return false;
+      }       
+    }
+var atPos = email.indexOf("@",1);
+      if (atPos == -1) {
+      return false;   
+    }
+      if (email.indexOf("@",atPos+1) != -1) {
+       return false;    
+    }
+var periodPos = email.indexOf(".",atPos);
+      if (periodPos == -1) {
+      return false;       
+    }
+    if (periodPos+3 > email.length) {
+      return false;   
+    } else {
+      return true;
+    }
+}
+//NUMBER DECIMAL FUNCTION
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     return {
+        "validEmail": validEmail
+        ""
+     }
+
+}
+
+var newLib = new myLibrary();
+
+    
+console.log(newLib.validEmail("bey@uty."));
+
+
+
+
