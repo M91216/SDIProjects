9f54fe7b3e5adf8319d2615752044b423dc4fa6e
Michael Bain <michaelbain@Michaels-MacBook-Pro.local>
2013-01-31 22:06:18 -0500

    days date function

--- Bain_Michael_assignment4.js
+++ Bain_Michael_assignment4.js
@@ -2,43 +2,41 @@
 // SDI project 4
 // 1/30/13
 
-var myLibrary = function() {
+    var myLibrary = function() {
 
 
 //EMAIL VALIDATION
-function validEmail(email) {
-    
-var invalidChars = " /:,;";
-    
+    function validEmail(email) {    
+    var invalidChars = " /:,;";    
     if (email ==""){       
-      return false;
+    return false;
     }
     for (var k=0; k<invalidChars.length; k++) {
-      var badChar = invalidChars.charAt(k);
-      if (email.indexOf(badChar) > -1){
-      return false;
-      }       
+    var badChar = invalidChars.charAt(k);
+    if (email.indexOf(badChar) > -1){
+    return false;
+    }       
     }
-var atPos = email.indexOf("@",1);
-      if (atPos == -1) {
-      return false;   
+    var atPos = email.indexOf("@",1);
+    if (atPos == -1) {
+    return false;   
     }
-      if (email.indexOf("@",atPos+1) != -1) {
-       return false;    
+    if (email.indexOf("@",atPos+1) != -1) {
+    return false;    
     }
-var periodPos = email.indexOf(".",atPos);
-      if (periodPos == -1) {
-      return false;       
+    var periodPos = email.indexOf(".",atPos);
+    if (periodPos == -1) {
+    return false;       
     }
     if (periodPos+3 > email.length) {
-      return false;   
+    return false;   
     } else {
-      return true;
+    return true;
     }
-}
+    }
 //NUMBER DECIMAL FUNCTION
 
-function fixMoney(getMoney) {
+    function fixMoney(getMoney) {
     var value = getMoney
     value.toFixed(2)
     return console.log(value.toFixed(2));
@@ -46,57 +44,67 @@ function fixMoney(getMoney) {
 
 //TITLE-CASE FUNCTION
 
-function wordUpper(val) {
+    function wordUpper(val) {
     newVal = ' ';
     val = val.toLowerCase().split(' ');
     for (var i=0; i < val.length; i++) {
-            newVal += val[i].substring(0,1).toUpperCase() + val[i].substring(1,val[i].length) + ' ';           
+    newVal += val[i].substring(0,1).toUpperCase() + val[i].substring(1,val[i].length) + ' ';           
     }
     return newVal;
-}
+    } 
 
 //STRING TO NUMBER FUNCTION
-function stringToNum(getNum) {
+    function stringToNum(getNum) {
     var value = getNum
     newVal = parseFloat(getNum);
     return newVal;
 
-}
+    }
 
-//TOTAL VALUE OF NUMBERS IN AN ARRAY
+//PHONE NUMBER VALIDATE
+    function validPhone(phoneNumber) {
+    if (phoneNumber != "") {
+    var validChars = "+- 1234567890()"
+    for (i = 0; i < phoneNumber.length; i++) {   
+    var c = phoneNumber.charAt(i);
+    if (validChars.indexOf(c) < 0) return false;
+    }
+    return true;
+    } else {
+    return false;
+    }
+    }
 
+        
+//DAYS BETWEEN DATES
 
+    var daysPassed = function(date) {
+    birthday = new Date(2012, 8, 13);
+    today = new Date(2013, 8, 13);
+    var days = 1000*60*60*24
+    console.log(Math.ceil((today.getTime() - birthday.getTime())/ (days)));
+    }
 
+ 
 
+    
 
-
-
-
-
-
-
-
-
-     return {
+    return {
         "validEmail"    : validEmail,
         "fixMoney"      : fixMoney,
         "wordUpper"     : wordUpper,
-        "stringToNum"   : stringToNum
+        "stringToNum"   : stringToNum,
+        "validPhone"    : validPhone,
+        "daysPassed"    : daysPassed
+    }                    
+
     }
 
-}
-
 var newLib = new myLibrary();
-
-
-
-
-
-
-
-
     
 console.log(newLib.validEmail("bey@uty."));
 console.log(newLib.fixMoney(75454));
 console.log(newLib.wordUpper("colin kapernick"));
 console.log(newLib.stringToNum("756748477"));
+console.log(newLib.validPhone(646464));
+console.log(newLib.daysPassed(2012, 8, 13));
